120ff41eed6dcf51ec8cce31d594084412bd5f5d
chihiro1013 <2638420181@qq.com>
2022-06-22 13:35:27 +0800

    01_基础语句

---
+++ theKi/c++学习/array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int merge(int *arr1, int *arr2) {
+	vector<int> vec;
+	while (*arr1 || *arr2) {
+		if (*arr1 != 0) {
+			cout << *arr1 << endl;
+			vec.push_back(*arr1);
+			arr1 ++;
+		}
+		if (*arr2 != 0) {
+			cout << *arr2 << endl;
+			vec.push_back(*arr2);
+			arr2 ++;
+		}
+	
+	}
+	cout << vec[0]<< endl;
+	return 1;
+}
+int main(void) {
+	int array[10] = {1, 2, 3, 4, 5};
+	int array2[10] = {5, 6, 7, 8};
+	int matrix[2][2] = {{1, 2}, {3, 4}};
+	cout << * array << endl; // 1
+	cout << ** matrix + 2 << endl; // 3
+	merge(array, array2);
+	return 0;
+}
+
---
+++ theKi/c++学习/io.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	int a,b;
+	short c;
+	bool d = true;
+	cin >> a>>b >> c;
+	cout << a << b << c << d<< endl;
+	return 0;
+}
